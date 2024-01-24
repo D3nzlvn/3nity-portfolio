ce175966a8dfd24db5285a90ac907c70b74fbb53
D3nzlvn <95868259+D3nzlvn@users.noreply.github.com>
2024-01-24 19:33:08 +0800

    aaaa

--- script.js
+++ script.js
@@ -1,35 +1,43 @@
 gsap.fromTo(
-    '.loading-page',
-    { opacity: 1 },
+  '.loading-page',
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+  
+gsap.fromTo(
+    '.logo-name',
     {
+      y: 50,
       opacity: 0,
-      display: "none",
-      duration: 1.5,
-      delay: 3.5,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
     }
   );
-    
-  gsap.fromTo(
-      '.logo-name',
-      {
-        y: 50,
-        opacity: 0,
-      },
-      {
-        y: 0,
-        opacity: 1,
-        duration: 2,
-        delay: 0.5,
-      }
-    );
-  
-  
-  let counter = document.querySelector('h1');
-  let count = 1;
-  setInterval(()=>{
-      counter.innerText = count;
-      count++
-  
-      if(count > 5) location.replace('Welcome.html');
-  },1000);
-  
+
+// Select the <h1> element
+let counter = document.querySelector('h1');
+
+// Initialize a counter variable
+let count = 1;
+
+// Set up an interval to run a function every 1000 milliseconds (1 second)
+setInterval(()=>{
+
+    // Update the text content of the <h1> element with the current count
+    counter.innerText = count;
+
+    // Increment the count
+    count++
+    // Check if the count is greater than 5 If true, redirect to 'Welcome.html'
+    if(count > 5) location.replace('Welcome.html');
+},1000);
+
--- script1.js
+++ script1.js
@@ -1,14 +1,35 @@
+// Select all sections on the page
 let sections = document.querySelectorAll('section');
+// Select all navigation links inside the header
 let navLinks = document.querySelectorAll('header nav a');
+
+// Attach a scroll event listener to the window
 window.onscroll = () => {
+     // loop through each section
     sections.forEach(sec => {
+
+        // Get the current scroll position
         let top = window.scrollY;
+
+        // Calculate the offset of the section 
         let offset = sec.offsetTop - 150;
+
+         // Get the height of the section
         let height = sec.offsetHeight;
+
+        // Get the id attribute of the section
         let id = sec.getAttribute('id');
+
+        // Check if the current scroll position is within the section's range
         if(top >= offset && top < offset + height) {
+
+             // loop through each navigation link
             navLinks.forEach(links => {
+
+                // Remove the 'active' class from all navigation links
                 links.classList.remove('active');
+
+                // Add the 'active' class to the navigation link corresponding to the current section
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
         };
@@ -16,22 +37,36 @@ window.onscroll = () => {
 };
 
 
+
+// Get all input elements with the name "filter"
 const filterInputs = document.querySelectorAll('input[name="filter"]');
+// Get all elements with the class ".pic"
     const projects = document.querySelectorAll('.pic');
 
+// Loop through each filter input
     filterInputs.forEach(input => {
+
+        // Attach an event listener for the 'change' event on each input
         input.addEventListener('change', () => {
+
+            // Get the value of the selected category from the input
             const selectedCategory = input.value;
 
+             // Check if the selected category is "all"
             if (selectedCategory === 'all') {
+                 // If "all" is selected then display all projects
                 projects.forEach(project => {
                     project.style.display = 'block';
                 });
             } else {
+                // If a specific category is selected, loop through each project
                 projects.forEach(project => {
+                    // Check if the project has the selected category
                     if (project.classList.contains(selectedCategory)) {
+                        // If naka select sa specific category then display the project
                         project.style.display = 'block';
                     } else {
+                        // If not, hide the project
                         project.style.display = 'none';
                     }
                 });
